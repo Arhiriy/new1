00600c93c67d4875a1bdfa45aa523786e6220984
Cybersport <Cybersport@26kadr.internal>
2025-02-06 10:41:48 +0300

    new2

---
+++ components/shared/container.tsx
@@ -0,0 +1,17 @@
+import { Container } from './container';
+import * as React from 'react';
+
+interface Props {
+className?: string;
+}
+export const Header: React.FC<Props> = ({ className }) => {
+    return (
+        <header className={cn('border border-b', className)}>
+            <Container className='flex items-center justify-between py-8'>
+                <div>
+                    
+                </div>
+            </Container>
+        </header>
+    );
+}
