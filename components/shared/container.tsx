54c78803642659870fe141844dca63613bf66f79
Cybersport <Cybersport@26kadr.internal>
2025-02-07 12:30:13 +0300

    new3

--- components/shared/container.tsx
+++ components/shared/container.tsx
@@ -1,17 +1,10 @@
-import { Container } from './container';
-import * as React from 'react';
+import { cn } from '@/lib/utils';
+import React from 'react';
 
 interface Props {
-className?: string;
+  className?: string;
 }
-export const Header: React.FC<Props> = ({ className }) => {
-    return (
-        <header className={cn('border border-b', className)}>
-            <Container className='flex items-center justify-between py-8'>
-                <div>
-                    
-                </div>
-            </Container>
-        </header>
-    );
-}
+
+export const Container: React.FC<React.PropsWithChildren<Props>> = ({ className, children }) => {
+  return <div className={cn('mx-auto max-w-[1280px]', className)}>{children}</div>;
+};
