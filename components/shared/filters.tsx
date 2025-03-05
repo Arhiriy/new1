f85d8eae51b5aff516b8b6a0f386dd6c36df0393
Cybersport <Cybersport@26kadr.internal>
2025-03-05 14:51:52 +0300

    dtu

--- components/shared/filters.tsx
+++ components/shared/filters.tsx
@@ -20,7 +20,7 @@ export const Filters: React.FC<Props> = ({className}) => {
                 <div className="mt-5 border-y border-y-neutral-100 py-6 pb-7">
                     <h1 className="font-bold mb-3">Цена от и до:</h1>
                     <div className="flex gap-3 mb-5">
-                        <Input type="number" placeholder="0" min={0} max={10} defaultValue={0}/>
+                        <Input type="number" placeholder="0" min={0} max={5000} defaultValue={0}/>
                         <Input type="number" min={0} max={5000} placeholder="0" />
                     </div>
                 </div>
